b8556b233d1a6b991284bd66b096aaf95c9bcfaa
hripsimeghazaryan <43012545+hripsimeghazaryan@users.noreply.github.com>
2018-12-01 13:32:26 +0400

    Add files via upload

---
+++ project.js
@@ -0,0 +1,159 @@
+const questions = myQuestions;
+
+const all = function(){
+
+  console.log(questions);
+
+  function Random() {
+        let display = questions;
+        let questionAmount = 10;
+        let questionTracker = [];
+        let randomQuestion;
+        for (let i = 0; i < questionAmount; i++) {
+          do {
+           randomQuestion = Math.floor(Math.random() * questions.length);
+             } while (existingQuestions());
+
+            display.innerHTML += questions[randomQuestion] + '<br>';
+            questionTracker.push(randomQuestion);
+            console.log(questionTracker);
+
+         }
+
+
+        function existingQuestions() {
+          for (let i = 0; i < questionTracker.length; i++) {
+            if (questionTracker[i] === randomQuestion) {
+              return true;
+             }
+            }
+            return false;
+        }
+    };
+
+
+  function buildQuiz(){
+  // we'll need a place to store the HTML output
+    const output = [];
+  // for each question
+    questions.forEach(
+      function(currentQuestion,questionNumber){
+        const answers = [];
+        // we'll want to store the list of answers choices
+        for(letter in currentQuestion.answers){ 
+  //for/in - loops through the properties of an object
+  // at that time currentQuestion is object with the answers
+  // ... add an HTML radio button
+  answers.push(
+    `<label>
+          <input type = "radio" name = "question${questionNumber}" value = "${letter}">
+           ${letter} :
+           ${currentQuestion.answers[letter]}
+    </label>`
+    );
+  // When a user clicks on a radio-button, it becomes checked, and all other radio-buttons with equal name become unchecked
+}
+  output.push(
+    `<div class = "slide">
+          <div class = "question"> ${currentQuestion.question} </div>
+          <div class = "answers"> ${answers.join("")}</div>
+     </div>`
+    );
+      });
+
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join("");
+  //The join() method joins the elements of an array into a string, and returns the string.
+}
+
+  function showResults(){
+   
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    let numCorrect = 0;
+
+    questions.forEach(function(currentQuestion,questionNumber){
+
+        const rightAnswers = answerContainers[questionNumber];
+        const selector = `input [name = question${questionNumber}]:checked`;
+        const userAnswer = (rightAnswers.querySelector(selector) || {}).value;
+  //The value property sets or returns the value of the value attribute of a text field.
+
+    if  (userAnswer === currentQuestion.correctAnswer){
+
+      numCorrect ++;
+
+      answerContainers[questionNumber].style.color = "green";
+    
+    }
+    
+    else{
+
+      answerContainers[questionNumber].style.color = "red";
+    
+    }
+  });
+
+  resultsContainer.innerHTML = `${numCorrect} out of ${questions.length}`;
+  }
+
+  function showSlide(n){
+
+    slides[currentSlide].classList.remove("active-slide");
+    slides[n].classList.add("active-slide");
+    currentSlide = n;
+
+    if (currentSlide === 0){
+      nextButton.style.display = "none";
+    }
+    else{
+      previousButton.style.display = "inline-block";
+    }
+    if (currentSlide === slides.length - 1){
+      nextButton.style.display = "none";
+      submitButton.style.display = "inline-block";
+    }
+    else{
+      nextButton.style.display = "inline-block";
+      submitButton.style.display = "none";
+    }
+  }
+
+  function showNextSlide(){
+      showSlide(currentSlide + 1);
+    }
+  function showPreviousSlide(){
+      showSlide(currentSlide - 1);
+    }
+
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
+
+
+buildQuiz();
+
+Random();
+
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+
+showSlide(0);
+
+
+submitButton.addEventListener('click', showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+
+};
+
+
+
+
+//myQuestions[random(myQuestions.length)];
+//const colorArray = ["white" ,"violet", "thistle", "purple", "rebeccapurple", "plum", "orchid", "mediumpurple", "indigo", "darkviolet"];
+//color: colorArray [random(colorArray.length - 1)],
+all();
---
+++ questions.js
@@ -0,0 +1,369 @@
+const myQuestions = [
+  {
+    question: "What year Pirates of the Caribbean: The Curse of Black Pearl released?",
+    answers: {
+      a: "2005",
+      b: "2003",
+      c: "2009",
+      d: "I don't know"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Which of the followings is not a professor from Hogwarts?",
+    answers: {
+      a: "Severus Snape",
+      b: "Gilderoy Lockhart",
+      c: "Rouben Meschian",
+      d: "I don't know"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who said, My Precious?",
+    answers: {
+      a: "Frodo",
+      b: "Gollum",
+      c: "Biblo",
+      d: "I don't know"
+    },
+    correctAnswer: "b"
+  },
+    {
+    question: "Which of the followings is not Steven Spielberg movie?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+    {
+    question: "Which movie won one big Oscar and seven little ones?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+    {
+    question: "Which one is Walt Disney's last work?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Who received Oscar after his death?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Put in the right order:",
+    answers: {
+      a: "Billionaire, genius, playboy, philathropist",
+      b: "Genius, playboy, billionaire, philathropist",
+      c: "Genius, billionaire, playboy, philathropist",
+      d: "I don't know"
+    },
+    correctAnswer: "c"
+  },
+   {
+    question: "Which of the followings is not James Bond actor?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Who does the voice over for Dory from Finding Nemo and Finding Dory?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "What famous actor is known for the saing, I'll be back?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "What was the name of the monkey in the Disney movie Aladdin?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "What is the only mythological animal mentioned in a Harry Potter book and movie title?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Which horror movie character is a serial killer possessing the doll?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Which character killed people in their dreams?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Which singer starred in The Bodyguard?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Who does the voice over for Woddy from Toy Story trilogy?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Who is the author of the songs from The Lion King?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Who said Why So Serious?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "In The Lion King, what was Simaba's father's name?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "What is the name of the magical board game that can only be stopped by finishing the game?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Which song plays in every Disney movie opening scene?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Which movie won the Academy Award in 2017?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+ {
+    question: "Which 1997 science fiction movie starring Will Smith and Tommy Lee Jones was released?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "(The question that only Armenians can answer) What movie is shown in TV every Christmas?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Whose iconic phrase is I'm gonna make him an offer he can't refuse?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "In which year were the Academy Awards, or Oscar first presented?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "How many Oscars won Avatar?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Both Anthony Hopkins and Antonio Banderas portrayed which hero? ",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Which actor had to play Tony Stark from Iron Man?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Who supposed to play CAPTAIN Jack Sparrow?",
+    answers: {
+      a: "Jim Carrey",
+      b: "Michael Keaton",
+      c: "Christopher Walken",
+      d: "Actually, all of them"
+    },
+    correctAnswer: "d"
+  },
+   {
+    question: "How many mistakes NASA managers have found in Armageddon?",
+    answers: {
+      a: "at least 168",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "Who said My mama always said Life is like a box of chocolate. You never know what you're gonna get?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "The creators of Rick and Morty were influenced from which movie?",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+      d: "I don't know"
+    },
+    correctAnswer: ""
+  },
+   {
+    question: "",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+
+    },
+    correctAnswer: ""
+  },
+
+  ];
+
+
+
+
+
+  
